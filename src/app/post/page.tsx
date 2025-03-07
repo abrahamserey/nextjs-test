6932166384f8eac431ce905c7245d77465ea1558
Abraham Serey <abrahamserey@gmail.com>
2025-03-07 09:57:52 +0200

    fixed typescript error

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,10 +1,6 @@
-import { DatabaseOperations } from "@/src/database/operations";
 import { CreatePost } from "../components/Blog/CreatePost";
 
 export default async function Post() {
-
-  const operations = new DatabaseOperations();
-  const posts = await operations.getBlogPosts();
 
   return (
     <div>
